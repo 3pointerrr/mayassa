fa09849f2ce655e26fa510aa291692036c446925
Mahdi_Abdollahi(meow enjoyer) <mahdiabdollahi212@gmail.com>
2023-03-10 13:29:15 +0330

    test features

--- product/admin/category.py
+++ product/admin/category.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
 from product.models.category import Category
+from product.models.product import Product
 
 
+class ProductInlineAdmin(admin.StackedInline):
+    model = Product
+    extra = 1
+    show_change_link = True
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 
@@ -15,7 +20,7 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = (
         'title',
     )
-
+    inlines = (ProductInlineAdmin,)
     search_help_text = 'search by title'
 
     list_filter = (

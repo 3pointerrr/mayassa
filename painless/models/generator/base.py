2c774e11451b2458b2ba0ca5de2e3c295ed2f4ad
Mahdi_Abdollahi(meow enjoyer) <mahdiabdollahi212@gmail.com>
2023-03-15 15:59:36 +0330

    feat(generator) add get random obj to generator

--- painless/models/generator/base.py
+++ painless/models/generator/base.py
@@ -1,5 +1,5 @@
 import secrets
-
+from random import random
 
 from mimesis.locales import Locale
 from mimesis import(
@@ -24,8 +24,12 @@ class BaseDataGenerator:
         self.address = Address(getattr(Locale,locale.upper()))
 
     def get_random_secret(self,nbytes=20):
-        
         return secrets.token_urlsafe(nbytes)
     
     def get_random_sentences(self,total):
         return self.person.full_name()
+    
+    def get_random_obj(self,population):
+        return random.choice(population)
+    
+

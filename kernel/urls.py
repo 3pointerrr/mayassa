9d3d560a29795c03df4517a7ce57034b6bf36e63
Mahdi_Abdollahi(meow enjoyer) <mahdiabdollahi212@gmail.com>
2023-03-30 14:41:07 +0330

    add debug mode

--- kernel/urls.py
+++ kernel/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
+from decouple import config
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if config("DEBUG_MODE",default=False,cast = bool):
+
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root = settings.STATIC_ROOT
+    )
+    
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT
+    )

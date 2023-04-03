52b1f77ad621f6f9c0aba4ef5d1f560b484f1562
Mahdi_Abdollahi(meow enjoyer) <mahdiabdollahi212@gmail.com>
2023-04-03 22:16:36 +0330

    register tag

---
+++ product/admin/tag.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from product.models.tag import Tag
+from product.models.product import Product
+
+
+class ProductInlineAdmin(admin.StackedInline):
+    model = Product
+    extra = 1
+    show_change_link = True
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'created',
+        'modified',
+    )
+
+    search_fields = (
+        'title',
+    )
+    inlines = (ProductInlineAdmin,)
+    search_help_text = 'search by title'
+
+    list_filter = (
+        'created',
+        'modified',
+    )
+
+    save_on_top=True
